d4ea7c2afa1652fbd892f3b21cd9ade7f980db3f
mourad <chabour.mourad87@gmail.com>
2020-02-18 11:16:27 +0100

    add paginator doctrine

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -6,6 +6,7 @@ use App\Entity\Conference;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -41,18 +42,24 @@ class ConferenceController extends AbstractController
 
     /**
      * @Route("/conference/{id}", name="app.conference_show")
+     * @param Request    $request
      * @param Conference $conference
      *
      * @return Response
      */
-    public function show(Conference $conference): Response
+    public function show(Request $request, Conference $conference): Response
     {
 
+        $offset = max(0,$request->query->getInt('offset', 0));
+        $paginator =  $this->commentRepository->getCommentPaginator($conference, $offset);
+
         return $this->render(
-            "conference/show.html.twig",
+            'conference/show.html.twig',
             [
                 'conference'=> $conference,
-                'comments' => $this->commentRepository->findBy(['conference' => $conference], ['createdAt' => 'ASC']),
+                'comments' => $paginator,
+                'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+                'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE)
             ]
         );
     }
