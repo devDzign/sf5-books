99c2332775fc30bcd8479ec0cd477fa89be57459
mourad <chabour.mourad87@gmail.com>
2020-02-19 15:28:54 +0100

    test functionnel

--- src/Fixture/AppFixtures.php
+++ src/Fixture/AppFixtures.php
@@ -4,7 +4,69 @@
 namespace App\Fixture;
 
 
-class AppFixtures
+use App\Entity\Admin;
+use App\Entity\Comment;
+use App\Entity\Conference;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
+
+class AppFixtures extends Fixture
 {
 
+    /**
+     * @var EncoderFactoryInterface
+     */
+    private $encoder;
+
+    public function __construct(EncoderFactoryInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function load(ObjectManager $manager)
+    {
+        $amstardam = new Conference();
+
+        $amstardam
+            ->setCity('Amsterdam')
+            ->setYear('2020')
+            ->setIsInternational(true)
+            ;
+
+        $manager->persist($amstardam);
+
+
+        $paris = new Conference();
+        $paris
+            ->setCity('Paris')
+            ->setYear('2020')
+            ->setIsInternational(false);
+
+        $manager->persist($paris);
+
+        $comment1 = new Comment();
+
+        $comment1
+            ->setConference($amstardam)
+            ->setAuthor('Fabien')
+            ->setEmail('mourad@exemple.com')
+            ->setText('This was a great conference.');
+
+        $manager->persist($comment1);
+
+        $admin = new Admin();
+
+        $admin
+            ->setRoles(['ROLE_ADMIN'])
+            ->setUsername('admin')
+            ->setPassword($this->encoder->getEncoder(Admin::class)->encodePassword('admin', null));
+
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
 }
---
+++ tests/Controller/ConferenceControllerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ConferenceControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        echo $client->getResponse();
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Give your feedback!');
+    }
+
+    public function testConferencePage()
+    {
+        $client = static::createClient();
+
+        $crawler  =  $client->request('GET', '/');
+
+        $this->assertCount(2, $crawler->filter('h4'));
+
+        $client->clickLink('View');
+
+        echo $client->getResponse();
+
+        $this->assertPageTitleContains('Amsterdam');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Amsterdam 2020');
+        $this->assertSelectorNotExists('div:contains("there are 1 comments")');
+    }
+}
